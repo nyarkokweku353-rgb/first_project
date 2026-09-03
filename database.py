37a350d731839e0a3cc2daabfbd91a329c50db18
Kweku Nyarko <nyarkokweku353@gmail.com>
2026-09-03 20:36:39 +0000

    first-project

---
+++ database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DATABASE_URL = 'sqlite:///example.db'
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+Base = declarative_base()
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hash_password = Column(String, nullable=False)
+
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, Form, status, HTTPException, Request, Depends
+from fastapi.templating import Jinja2Templates
+from database import User, get_db
+from sqlalchemy.orm import Session
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+#from passlib.context import CryptContext
+
+#pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+app = FastAPI()
+templates = Jinja2Templates(directory="template")
+app.mount("/static", StaticFiles(directory="template"), name="static")
+
+@app.get("/signup", response_class=HTMLResponse)
+def get_signup_page(request: Request):
+    return templates.TemplateResponse("signup.html", {"request": request})
+
+@app.post("/signup")
+def create_user(request: Request, email: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
+    #hashed_password = pwd_context.hash(password)
+
+    existing_user = db.query(User).filter(User.email == email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    new_user = User(email=email, hash_password=password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return templates.TemplateResponse("signup.html", {"request": request, "user": new_user})
+
+#from fastapi.staticfiles import StaticFiles
+#app.mount("/static", StaticFiles(directory="static"), name="static")
