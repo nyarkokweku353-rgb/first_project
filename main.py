2b86072ee03a5a174a0fb79debb9cc4c281304a6
Kweku Nyarko <nyarkokweku353@gmail.com>
2026-09-15 00:24:52 +0000

    initial

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from database import User, get_db
 from sqlalchemy.orm import Session
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
-#from passlib.context import CryptContext
+from passlib.context import CryptContext
 
-#pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 app = FastAPI()
 templates = Jinja2Templates(directory="template")
 app.mount("/static", StaticFiles(directory="template"), name="static")
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 @app.get("/signup", response_class=HTMLResponse)
 def get_signup_page(request: Request):
@@ -17,13 +18,13 @@ def get_signup_page(request: Request):
 
 @app.post("/signup")
 def create_user(request: Request, email: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
-    #hashed_password = pwd_context.hash(password)
+    hashed_password = pwd_context.hash(password)
 
     existing_user = db.query(User).filter(User.email == email).first()
     if existing_user:
         raise HTTPException(status_code=400, detail="Email already registered")
 
-    new_user = User(email=email, hash_password=password)
+    new_user = User(email=email, hash_password=hashed_password)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
@@ -31,4 +32,6 @@ def create_user(request: Request, email: str = Form(...), password: str = Form(.
     return templates.TemplateResponse("signup.html", {"request": request, "user": new_user})
 
 #from fastapi.staticfiles import StaticFiles
-#app.mount("/static", StaticFiles(directory="static"), name="static")
+#app.mount("/static", StaticFiles(directory="static"), name="static")   
+
+
