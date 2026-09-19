47152dbe516c75922f3ee8ac98bd8545862ccd7b
Kweku Nyarko <nyarkokweku353@gmail.com>
2026-09-19 03:31:24 +0000

    Fix everything

--- main.py
+++ main.py
@@ -12,26 +12,29 @@ app.mount("/static", StaticFiles(directory="template"), name="static")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-@app.get("/signup", response_class=HTMLResponse)
+@ app.get("/signup", response_class=HTMLResponse)
 def get_signup_page(request: Request):
     return templates.TemplateResponse("signup.html", {"request": request})
 
-@app.post("/signup")
-def create_user(request: Request, email: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
+@ app.post("/signup")
+def create_user(request: Request,
+                 email: str = Form(...), 
+                 password: str = Form(...), 
+                 db: Session = Depends(get_db)):
     hashed_password = pwd_context.hash(password)
 
     existing_user = db.query(User).filter(User.email == email).first()
     if existing_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
+        raise HTTPException(status_code=400,
+                             detail="Email already registered")
 
     new_user = User(email=email, hash_password=hashed_password)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
 
-    return templates.TemplateResponse("signup.html", {"request": request, "user": new_user})
+    return templates.TemplateResponse("signup.html",
+                                       {"request": request, "user": new_user})
 
-#from fastapi.staticfiles import StaticFiles
-#app.mount("/static", StaticFiles(directory="static"), name="static")   
-
-
+# from fastapi.staticfiles import StaticFiles
+# app.mount("/static", StaticFiles(directory="static"), name="static")
