59e5d99d4757a7e51421251c2c9e252043a8cd4f
Kweku Nyarko <nyarkokweku353@gmail.com>
2026-09-21 03:01:23 +0000

    Fix everything

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 @app.get("/signup", response_class=HTMLResponse)
 def get_signup_page(request: Request):
-    return templates.TemplateResponse("signup.html", request)
+    return templates.TemplateResponse(request, "signup.html", {"user": None})
 
 
 @app.post("/signup")
@@ -32,7 +32,8 @@ def create_user(request: Request, email: str = Form(...),
     db.commit()
     db.refresh(new_user)
 
-    return templates.TemplateResponse(request, "signup.html", {"user": new_user})
+    return templates.TemplateResponse(request, "signup.html", 
+                                      {"user": new_user})
 
 # from fastapi.staticfiles import StaticFiles
 # app.mount("/static", StaticFiles(directory="static"), name="static")
